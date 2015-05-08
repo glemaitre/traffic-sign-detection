e33ade48458f02307dc7ee2313388c1297f7f911
Guillaume Lemaitre <glemaitre@visor.udg.edu>
2015-05-08 22:09:05 +0200

    Change licence to BSD 3

--- src/common/SuperFormula.cpp
+++ src/common/SuperFormula.cpp
@@ -1,21 +1,45 @@
 /*
-* Copyright (c) 2015 
-* Guillaume Lemaitre (g.lemaitre58@gmail.com)
-* Yohan Fougerolle (Yohan.Fougerolle@u-bourgogne.fr)
-*
-* This program is free software; you can redistribute it and/or modify it
-* under the terms of the GNU General Public License as published by the Free
-* Software Foundation; either version 2 of the License, or (at your option)
-* any later version.
-*
-* This program is distributed in the hope that it will be useful, but WITHOUT
-* ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-* FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for
-* more details.
-*
-* You should have received a copy of the GNU General Public License along
-* with this program; if not, write to the Free Software Foundation, Inc., 51
-* Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA
+
+By downloading, copying, installing or using the software you agree to this license.
+If you do not agree to this license, do not download, install,
+copy or use the software.
+
+
+                          License Agreement
+               For Open Source Computer Vision Library
+                       (3-clause BSD License)
+
+Copyright (C) 2015, 
+	  Guillaume Lemaitre (g.lemaitre58@gmail.com), 
+	  Johan Massich (mailsik@gmail.com),
+	  Gerard Bahi (zomeck@gmail.com),
+	  Yohan Fougerolle (Yohan.Fougerolle@u-bourgogne.fr).
+Third party copyrights are property of their respective owners.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted provided that the following conditions are met:
+
+  * Redistributions of source code must retain the above copyright notice,
+    this list of conditions and the following disclaimer.
+
+  * Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
+
+  * Neither the names of the copyright holders nor the names of the contributors
+    may be used to endorse or promote products derived from this software
+    without specific prior written permission.
+
+This software is provided by the copyright holders and contributors "as is" and
+any express or implied warranties, including, but not limited to, the implied
+warranties of merchantability and fitness for a particular purpose are disclaimed.
+In no event shall copyright holders or contributors be liable for any direct,
+indirect, incidental, special, exemplary, or consequential damages
+(including, but not limited to, procurement of substitute goods or services;
+loss of use, data, or profits; or business interruption) however caused
+and on any theory of liability, whether in contract, strict liability,
+or tort (including negligence or otherwise) arising in any way out of
+the use of this software, even if advised of the possibility of such damage.
 */
 
 #include "math_utils.h"
